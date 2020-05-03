703d0883bef3ca236a4377e6ece9dedaa726f10d
acer <acer@acer.mynet>
2020-05-03 17:46:01 +0100

    Added ratings data service

---
+++ ratings-data-service/src/main/java/com/my/ratingsdataservice/RatingsDataResource.java
@@ -0,0 +1,32 @@
+package com.my.ratingsdataservice;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.my.model.Rating;
+import com.my.model.UserRatings;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsDataResource {
+
+	@RequestMapping("/{movieId}")
+	public Rating getRatingsData(@PathVariable("movieId") String movieId) {
+		return new Rating(movieId, 4);
+	}
+	
+	@RequestMapping("users/{userId}")
+	public UserRatings getRatings(@PathVariable("userId") String userId) {
+		List<Rating> ratings = Arrays.asList(
+				new Rating("1234", 4),
+				new Rating("5678", 3)
+		);
+		UserRatings userRatings = new UserRatings();
+		userRatings.setRatings(ratings);
+		return userRatings;
+	}
+}
